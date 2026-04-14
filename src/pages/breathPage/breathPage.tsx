be482b3d22a0cf0c0093a8e70a237236b54e2d74
Mirandahn97 <mira0249@elev.techcollege.dk>
2026-04-14 09:07:58 +0200

    v2

--- src/pages/breathPage/breathPage.tsx
+++ src/pages/breathPage/breathPage.tsx
@@ -1,7 +1,15 @@
+import style from './breathPage.module.scss'
+
 export function BreathPage() {
 
-    return(
+    return (
         <>
+            <div className={style.dotContainer}>
+                <div className={style.backgroundGradient}></div>
+                <p className={style.test}>Breath</p>
+                <div className={style.dot}></div>
+            </div>
+
         </>
     )
 }
