163352d4a59cf49992b6c28d747bea7d7fbad169
Mirandahn97 <mira0249@elev.techcollege.dk>
2026-04-14 12:46:25 +0200

    v6

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+if('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('./sw.js')
+	.then(reg => console.log('service worker registered', reg))
+	.catch(err => console.error('service worker not registered', err)) 
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
